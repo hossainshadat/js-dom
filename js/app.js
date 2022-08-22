6145007a1619890abd723de41973d6709681eff6
hossainshadat <hossainshadat501@gmail.com>
2022-08-22 21:33:09 +0600

    Selected Player Logic also done

--- js/app.js
+++ js/app.js
@@ -16,13 +16,17 @@ function selectBtn(btn) {
   for (let i = 0; i < selectPlayer.length; i++) {
     const tr = document.createElement("tr");
 
-    tr.innerHTML = `
-  <th>${i + 1}</th>
-  <td>${selectPlayer[i].title}</td>`;
+    if (selectPlayer.length <= 5) {
+      tr.innerHTML = `
+      <th>${i + 1}</th>
+      <td>${selectPlayer[i].title}</td>`;
+      btn.classList.add("btn-disabled");
+    } else {
+      alert("Please select maximum 5 Player ");
+      break;
+    }
 
     selectedPlayer.appendChild(tr);
   }
-  btn.classList.add("btn-disabled");
-
   calcPerPlayerExpense(selectPlayer);
 }
