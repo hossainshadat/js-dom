de34ea5a64fc839c9174635b626aadb14383d060
hossainshadat <hossainshadat501@gmail.com>
2022-08-22 08:23:26 +0600

    Select Player done

--- js/app.js
+++ js/app.js
@@ -1,1 +1,28 @@
-cl;
+let selectPlayer = [];
+
+function selectBtn(btn) {
+  const title = btn.parentNode.parentNode.children[0].innerText;
+
+  const selectedPlayer = document.getElementById("selected-player");
+  selectedPlayer.innerText = "";
+
+  const playerName = {
+    title: title,
+  };
+
+  selectPlayer.push(playerName);
+
+  for (let i = 0; i < selectPlayer.length; i++) {
+    console.log(selectPlayer[i]);
+
+    const tr = document.createElement("tr");
+
+    tr.innerHTML = `
+  <th>${i + 1}</th>
+  <td>${selectPlayer[i].title}</td>`;
+
+    selectedPlayer.appendChild(tr);
+  }
+
+  console.log(selectPlayer.length);
+}
