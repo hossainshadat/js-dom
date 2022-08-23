fbf84afd57bc7d5079d7719fac271220ba9881cb
hossainshadat <hossainshadat501@gmail.com>
2022-08-23 19:53:31 +0600

    update common js file

--- js/app.js
+++ js/app.js
@@ -3,31 +3,12 @@ let selectPlayer = [];
 function selectBtn(btn) {
   const title = btn.parentNode.parentNode.children[0].innerText;
 
-  // const selectedPlayer = document.getElementById("selected-player");
-
-  // selectedPlayer.innerText = "";
-
   const playerName = {
     title: title,
   };
 
   selectPlayer.push(playerName);
 
-  // for (let i = 0; i < selectPlayer.length; i++) {
-  //   const tr = document.createElement("tr");
-
-  //   if (selectPlayer.length <= 5) {
-  //     tr.innerHTML = `
-  //     <th>${i + 1}</th>
-  //     <td>${selectPlayer[i].title}</td>`;
-  //     btn.classList.add("btn-disabled");
-  //   } else {
-  //     alert("Please select maximum 5 Player ");
-  //     break;
-  //   }
-
-  //   selectedPlayer.appendChild(tr);
-  // }
   selectPlayerLoop(btn, selectPlayer);
   calcPerPlayerExpense(selectPlayer);
 }
--- js/common.js
+++ js/common.js
@@ -1,18 +1,19 @@
 function selectPlayerLoop(btn, selectPlayer) {
   const selectedPlayer = document.getElementById("selected-player");
 
-  selectedPlayer.innerText = "";
+  // selectedPlayer.innerText = "";
+
   for (let i = 0; i < selectPlayer.length; i++) {
     const tr = document.createElement("tr");
 
-    if (selectPlayer.length <= 5) {
+    if (!(selectPlayer.length <= 5)) {
+      alert("Please select maximum 5 Player ");
+      break;
+    } else {
       tr.innerHTML = `
         <th>${i + 1}</th>
         <td>${selectPlayer[i].title}</td>`;
       btn.classList.add("btn-disabled");
-    } else {
-      alert("Please select maximum 5 Player ");
-      break;
     }
 
     selectedPlayer.appendChild(tr);
